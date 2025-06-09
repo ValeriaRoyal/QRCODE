b480b4e3419eb74c9a1df08641e7ff16db53d42d
Valéria <valeria@exemplo.com>
2025-06-09 18:27:31 -0300

    Melhorias no app: correção de bug na exibição do texto escaneado, adição de conteúdo nas páginas About e Contact, e melhor tratamento de erros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,13 +27,21 @@ export class HomePage {
       this.scannedData = data;
     }, (err) => {
       console.log('Erro: ', err);
+      alert('Erro ao escanear: ' + err);
     })
   }
   encode() {
+    if (!this.encodText) {
+      alert('Por favor, digite um texto para gerar o QR Code');
+      return;
+    }
+    
     this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data) => {
       this.encodeData = data;
+      console.log('QR Code gerado com sucesso!');
     }, (err) => {
       console.log('Erro: ', err);
+      alert('Erro ao gerar QR Code: ' + err);
     })
   }
 }
